805347032b874e8af5df9427777af3169533166f
Zayn Otley <intuitionamiga@gmail.com>
2017-09-25 10:41:51 +0100

    armv8 build script for termux

---
+++ build_armv8.sh
@@ -0,0 +1,20 @@
+rm ./bin/gav*
+
+#echo Building with GCC
+#gcc -Wall -Wextra -Ofast -m64 -march=native -fwhole-program -fomit-frame-pointer -fPIC -pipe -fexpensive-optimizations gav.c -o ./bin/gav_gcc
+#strip -S ./bin/gav_gcc
+#time ./bin/gav_gcc>./bin/gav.ppm
+#echo GCC results
+
+#echo
+
+echo Building with Clang
+clang -Wall -Wextra -Ofast -m64 -march=armv8 -fomit-frame-pointer -fPIC -pipe gav.c -o ./bin/gav_clang
+strip -S ./bin/gav_clang
+time ./bin/gav_clang>gav.ppm
+convert gav.ppm gav.png
+cp gav.png /sdcard/
+echo Clang results
+
+#display ./bin/gav.ppm
+
